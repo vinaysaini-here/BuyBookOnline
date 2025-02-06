6a9083aa483ef6e859f71006806c214bedcda12b
vinaysaini-here <vinaysaini675859@gmail.com>
2025-02-06 10:34:53 +0530

    Production ready

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE ==="development" ? "http://localhost:8000" : "/api" ,// Your API base URL
+  baseURL:
+    import.meta.env.MODE === "development" ? "http://localhost:8000" : "/", // Your API base URL
   withCredentials: true, // Allows sending cookies with requests
 });
-
 
 export { axiosInstance };
